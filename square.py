a5e64852dc654ce5f9d5a886a518ffe6fe377223
HimmelsbachJannik <Jannik.Himmelsbach@enroutesystems.com>
2022-01-12 14:14:40 -0600

    minor changes in sqare.py

--- square.py
+++ square.py
@@ -1,29 +1,10 @@
 from rectangle import Rectangle
-from dataclasses import dataclass
-import math
-
-@dataclass
 class Square(Rectangle):
-    # side: float = 1.0
-    # width: float = 1.0
-    # length: float = 1.0
-    # color: str
 
     def __init__(self, side=1.0, color='red', filled=True): 
         self.setSide(side)
         self.setColor(color)
         self.setFilled(filled)
-
-        # if (width or length): 
-            
-            # if (color and filled): 
-            #     self.width=width 
-            #     self.length=width 
-            #     self._color = color
-            #     self.setFilled(filled)
-            # else: 
-            #     self.width=width 
-            #     self.length=width
 
     def getSide(self):
         return self.width
@@ -45,5 +26,5 @@ class Square(Rectangle):
         return self.width * 4
     
     def __str__(self):
-        return f'Square color: {self.getColor()}, Square filled: {self._filled}, ' \
+        return f'Square color: {self.getColor()}, Square filled: {self.isFilled()}, ' \
             f'width: {self.width}, length: {self.width}'
